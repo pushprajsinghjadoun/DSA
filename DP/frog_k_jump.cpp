2d91b61dc8a1bf6022f6ee201f4ea8581a45c8fa
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-04-02 15:00:54 +0530

    adding another program

---
+++ DP/frog_k_jump.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int ind,vector<int>&v,vector<int>&dp,int k)
+{
+    if(ind==0)
+    {
+        return 0;
+    }
+    if(dp[ind]!=-1) return dp[ind];
+    int minsub =INT_MAX;
+    for(int i=1;i<=k;i++)
+    {
+        int temp = INT_MAX;
+        if(ind-i>=0)
+        {
+            temp = f(ind-1,v,dp,k)+ abs(v[ind]-v[ind-i]);
+        }
+        minsub = min(minsub,temp);
+    }
+    cout<<minsub<<" . "<<ind<<endl;
+    dp[ind] = minsub;
+    return minsub;
+
+}
+
+frog(int n,vector<int> &v,int k)
+{
+    vector<int> dp(n,-1);
+    return f(n-1,v,dp,k);
+}
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    cout<<frog(n,v,k)<<endl;
+    return 0;
+}
---
+++ DP/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+abs(v[ind]-v[ind-1]
